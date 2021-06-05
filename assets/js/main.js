d296dc17c81964383836d95506f1c36823867e74
Josiah <josiah.james.fowler@gmail.com>
2021-06-05 17:51:36 -0400

    scssAdded

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,10 @@ checkbox.addEventListener("change", () => {
         body.classList.remove("dark-mode");
     }
 });
+
+let root = document.documentElement;
+
+root.addEventListener("mousemove", e => {
+    root.style.setProperty('--mouse-x', e.clientX + "px");
+    root.style.setProperty('--mouse-y', e.clientY + "px");
+});
