0a8c7f1a3ec39f7c57390cad359d4bf9a18fcc42
Josiah <josiah.james.fowler@gmail.com>
2021-06-07 18:18:02 -0400

    stylesUpdated

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,9 +10,3 @@ checkbox.addEventListener("change", () => {
     }
 });
 
-let root = document.documentElement;
-
-root.addEventListener("mousemove", e => {
-    root.style.setProperty('--mouse-x', e.clientX + "px");
-    root.style.setProperty('--mouse-y', e.clientY + "px");
-});
